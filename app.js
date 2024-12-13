1fa88a1bfc8468e30864f2eac9b365b75c307a78
김련호 <lyunho.kim@flyground.co.kr>
2024-12-14 00:17:42 +0900

    health check API 추가

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
+  console.log("get request received");
   res.send("API Server is on successfully");
 });
 
-app.listen(3000);
+app.get("/health", (req, res) => {
+  console.log("health check request received");
+  res.send("API Server is healthy");
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000 successfully");
+});
