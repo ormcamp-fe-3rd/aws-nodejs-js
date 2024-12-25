247678ac336691ae3d3cb96ee3afdb7cc5f5bfcb
김련호 <lyunho.kim@flyground.co.kr>
2024-12-26 00:13:19 +0900

    DB 환경 변수 적용

--- 3.db_server.js
+++ 3.db_server.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 // mysql 연결
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "123qwer@",
-  database: "sampledb",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 // DB 생성
